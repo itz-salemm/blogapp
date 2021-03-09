88cd8095d7f214771ea301a805acc4d9ecb99ba0
It_salemm <salemolorundare@gmail.com>
2021-03-09 00:58:34 -0800

    finished setting up

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+class Content(models.Model):
+    title = models.CharField(max_length=20)
+    text = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    authur = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from . models import Content
 
+from django.views.generic import ListView
 # Create your views here.
+
+def homeView(request):
+    context = {}
+    return render(request, 'blog/index.html', context)
+    
